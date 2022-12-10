a18009c68a0bf1deb30392abcfd0d9eb64c37843
Mostafa Rahimi <70751044+mocrahimi@users.noreply.github.com>
2022-12-11 01:32:06 +0330

    Adding Network functionality

--- DNS_Switcher/MainWindow.xaml.cs
+++ DNS_Switcher/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using ControlzEx.Standard;
 using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace DNS_Switcher
@@ -16,6 +21,9 @@ namespace DNS_Switcher
             InitializeComponent();
         }
         Animations anim = new Animations();
+        Network net = new Network();
+
+        //UI page change Animation
         private void transaction(FrameworkElement element)
         {
 
@@ -66,5 +74,88 @@ namespace DNS_Switcher
         {
             transaction(SpeedCheck_Page);
         }
+
+        private void MainBackground_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshUiInfo();
+        }
+        public void GetPing(string IP)
+        {
+            Thread.Sleep(2000);//for better visual animation
+            long totalTime = 0;
+            int timeout = 120;
+            Ping pingSender = new Ping();
+
+            for (int i = 0; i < 16; i++)
+            {
+                PingReply reply = pingSender.Send(IP, timeout);
+                if (reply.Status == IPStatus.Success)
+                {
+                    totalTime += reply.RoundtripTime;
+                }
+            }
+            this.Dispatcher.Invoke(() => {
+                 Ping_Label.Text = (totalTime / 16).ToString();
+                 Ping_Label.Visibility = Visibility.Visible;
+                 PingProgress.Visibility = Visibility.Collapsed;
+                 MS_Label.Visibility = Visibility.Visible;
+            });
+            
+            
+        }
+        //change dns to selected dns
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          if(DNS_Lists.SelectedItem == Shecan)
+          {
+                net.SetDNS("178.22.122.100", "185.51.200.2");
+                RefreshUiInfo();
+          }
+          else if(DNS_Lists.SelectedItem == Begzar){
+
+                net.SetDNS("185.55.226.26", "185.55.225.25");
+                RefreshUiInfo();
+            } 
+          else if (DNS_Lists.SelectedItem == Elctro){
+
+                net.SetDNS("78.157.42.100", "78.157.42.101");
+                RefreshUiInfo();
+            }
+          else if (DNS_Lists.SelectedItem == Cloudflare)
+            {
+
+                net.SetDNS("1.1.1.1", "1.0.0.1");
+                RefreshUiInfo();
+            } 
+          else if (DNS_Lists.SelectedItem == Google){
+
+                net.SetDNS("8.8.8.8", "8.8.4.4");
+                RefreshUiInfo();
+            }
+
+
+
+        }
+        //updating the UI
+        private void RefreshUiInfo()
+        {
+            List<string> result = new List<string>();
+            result = net.DisplayDnsAddresses();
+            DNS1.Text = result[0];
+            DNS2.Text = result[1];
+            this.Dispatcher.Invoke(() => {
+                Ping_Label.Visibility = Visibility.Collapsed;
+                PingProgress.Visibility = Visibility.Visible;
+                MS_Label.Visibility = Visibility.Collapsed;
+            });
+            Task.Run(() => GetPing(result[0]));
+        }
+
+        private void RemoveDNSButton_Click(object sender, RoutedEventArgs e)
+        {
+            net.RemoveDNS();
+            RefreshUiInfo();
+        }
     }
+    
 }
---
+++ DNS_Switcher/Network.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace DNS_Switcher
+{
+    class Network
+    {   //get active network adaptor
+       
+        public static NetworkInterface GetActiveEthernetOrWifiNetworkInterface()
+        {
+            var Nic = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(
+                a => a.OperationalStatus == OperationalStatus.Up &&
+                (a.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || a.NetworkInterfaceType == NetworkInterfaceType.Ethernet) &&
+                a.GetIPProperties().GatewayAddresses.Any(g => g.Address.AddressFamily.ToString() == "InterNetwork"));
+            return Nic;
+        }
+
+        //get dns of the active network adaptor
+        public List<string> DisplayDnsAddresses()
+        {
+            List<string> result = new List<string>();
+            var CurrentInterface = GetActiveEthernetOrWifiNetworkInterface();
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                if (adapter.Description == CurrentInterface.Description)
+                {
+                    IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                    IPAddressCollection dnsServers = adapterProperties.DnsAddresses;
+                    if (dnsServers.Count > 0)
+                    {
+                        
+                        foreach (IPAddress dns in dnsServers)
+                        {
+                            result.Add(dns.ToString());
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        //set user dns 
+        public void SetDNS(string firstdns,string secenddns)
+        {
+            string[] Dns = { firstdns, secenddns };
+            var CurrentInterface = GetActiveEthernetOrWifiNetworkInterface();
+            if (CurrentInterface == null) return;
+
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Description"].ToString().Equals(CurrentInterface.Description))
+                    {
+                        ManagementBaseObject objdns = objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                        if (objdns != null)
+                        {
+                            objdns["DNSServerSearchOrder"] = Dns;
+                            objMO.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+                        }
+                    }
+                }
+            }
+
+        }
+        //remove every dns and set it to auto ,a new dns will be asign by the router 
+        public void RemoveDNS()
+        {
+            var CurrentInterface = GetActiveEthernetOrWifiNetworkInterface();
+            if (CurrentInterface == null) return;
+
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Description"].ToString().Equals(CurrentInterface.Description))
+                    {
+                        ManagementBaseObject objdns = objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                        if (objdns != null)
+                        {
+                            objdns["DNSServerSearchOrder"] = null;
+                            objMO.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+                        }
+                    }
+                }
+            }
+        }
+        //check the ping to given ip address
+        public double GetPing(string IP)
+        {
+            long totalTime = 0;
+            int timeout = 120;
+            Ping pingSender = new Ping();
+
+            for (int i = 0; i < 16; i++)
+            {
+                PingReply reply = pingSender.Send(IP, timeout);
+                if (reply.Status == IPStatus.Success)
+                {
+                    totalTime += reply.RoundtripTime;
+                }
+            }
+            return totalTime / 16;
+        }
+        
+    }
+}
