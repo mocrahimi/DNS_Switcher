e0be36ee3450fc86ad0844e91526a0dc5b915d72
Mostafa Rahimi <70751044+mocrahimi@users.noreply.github.com>
2022-12-10 20:54:34 +0330

    Add project files.

---
+++ DNS_Switcher/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DNS_Switcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+           
+        }
+
+        private void ExitButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
